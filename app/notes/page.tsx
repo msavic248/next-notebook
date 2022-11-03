b00de1e8ffece16ce948ed2333e23b974b066006
Milos Savic <msavic248@yahoo.com>
2022-11-03 16:38:28 +0100

    Added notes page

---
+++ app/notes/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default async function NotesPage() {
+    return(
+        <div>
+            <h1>Notes</h1>
+        </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import "./globals.css"
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>Some content</p>
+        </div>
+    )
+}
