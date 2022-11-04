38a57b1947118d0f47e231c3ac6421bb7d6e621b
Milos Savic <msavic248@yahoo.com>
2022-11-04 16:34:51 +0100

    Added individual note page

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,26 @@
+import PocketBase from 'pocketbase';
+import "../../globals.css";
+import styles from "../Notes.module.css";
+
+async function getNote(noteId: string) {
+    const db = new PocketBase('http://127.0.0.1:8090');
+    const record = await db.records.getOne('notes', `${noteId}`, {
+        expand: 'some_relation'
+    });
+    return record;
+}
+
+export default async function NotePage({params}: any) {
+    const note = await getNote(params.id);
+
+    return(
+        <div>
+            <h1>notes/{note.id}</h1>
+            <div className={styles.note}>
+                <h2>{note.title}</h2>
+                <h5>{note.content}</h5>
+                <p>{note.created}</p>
+            </div>
+        </div>
+    )
+}
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,9 +1,50 @@
+import Link from "next/link";
+import PocketBase from 'pocketbase';
+import "../globals.css"
+import styles from "./Notes.module.css";
 
+export const dynamic = 'auto',
+  dynamicParams = true,
+  revalidate = 0,
+  fetchCache = 'auto',
+  runtime = 'nodejs',
+  preferredRegion = 'auto'
+
+async function getNotes() {
+
+    const db = new PocketBase('http://127.0.0.1:8090');
+    const data = await db.records.getList("notes");
+    // const res = await fetch("http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30",
+    // {cache: "no-store"});
+    // const data = await res.json();
+    return data?.items as any[];
+}
 
 export default async function NotesPage() {
+    const notes = await getNotes();
+
     return(
         <div>
             <h1>Notes</h1>
+            <div className={styles.link}>
+                {notes?.map(note => {
+                    return <Note key={note.id} note={note} />;
+                })}
+            </div>
         </div>
+    )
+}
+
+function Note({note}: any) {
+    const {id, title, content, created} = note || {};
+
+    return (
+        <Link href={`/notes/${id}`}>
+            <div className={styles.note}>
+                <h2>{title}</h2>
+                <h5>{content}</h5>
+                <p>{created}</p>
+            </div>
+        </Link>
     )
 }
